add3c85670d6f1eec853bb23153af5217d976e59
VAmpersand <VP_ampersand_P@icloud.com>
2020-06-22 20:04:22 +0300

    Initial Commit

---
+++ AppveloxTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppveloxTestProject
+//
+//  Created by Viktor on 22.06.2020.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
